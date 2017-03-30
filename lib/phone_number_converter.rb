1f92e88e3570b8275ceefd99b3d1966f256fca56
shobhit.shukla <shobhit.shukla@wwindia.com>
2017-03-30 12:04:16 +0530

    Initial commit.

---
+++ lib/phone_number_converter.rb
@@ -0,0 +1,31 @@
+class PhoneNumberConverter
+
+  PATTERN = [
+    [3,3,4],
+    [3,4,3],
+    [4,3,3],
+    [5,5],
+    [4,6],
+    [6,4],
+    [7,3],
+    [3,7],
+    [10]
+  ]
+
+  NUMBER_LETTERS = {
+    "2" => ["A", "B", "C"],
+    "3" => ["D", "E", "F"],
+    "4" => ["G", "H", "I"],
+    "5" => ["J", "K", "L"],
+    "6" => ["M", "N", "O"],
+    "7" => ["P", "Q", "R", "S"],
+    "8" => ["T", "U", "V"],
+    "9" => ["W", "X", "Y", "Z"]
+  }
+
+  def initialize phonenumber
+    @phonenumber = phonenumber.chars
+  end
+
+  phone_number_converter = PhoneNumberConverter.new "1282668687"
+end
